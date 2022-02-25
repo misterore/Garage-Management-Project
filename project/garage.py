38b5b951c6b9384ac50ef0c0c319ada112cce51e
misterore <ijamoaina@gmail.com>
2022-02-25 02:25:08 -0500

    changed in operator and fixed input bugs

--- project/garage.py
+++ project/garage.py
@@ -4,23 +4,41 @@ import re
 garage = [None] * 11
 
 
+def none_checker():
+    i = 0
+    while i <= len(garage):
+        if garage[i] is None:
+            return i, True
+        i += 0
+
+
 def parking(license):
     matched = re.match(
         "[A-Z][A-Z][A-Z][A-Z][-][0-9][0-9][-][0-9][0-9][0-9]", license)
     is_match = bool(matched)
+    if len(license) != 12:
+        is_match = False
+
+    k = 0
+    j = 0
+
     if is_match is True:
-        print("License plate number correct.\n")
-        if license in garage:
-            print("Car already in the garage.")
-        else:
-            print("Parking...\n")
-            for i in range(len(garage) + 1):
-                if garage[i] == None:
-                    garage[i] = license
+        print("\nLicense plate number correct.\n")
+        while j < len(garage):
+            if license == garage[j]:
+                print("Car already in the garage.")
+                break
+            j += 1
+            while k < len(garage):
+                if garage[k] == None:
+                    print("Parking...\n")
+                    garage[k] = license
+                    print("Completed! See you soon.")
                     break
-            print("Completed! See you soon.")
+                k += 1
+            break
     if is_match is False:
-        print("License Incorrect.")
+        print("\nLicense Incorrect.\n")
         license = str(input(
             "Please re-enter your license plate number in ABCD-12-3456 format: "))
         parking(license)
@@ -30,17 +48,27 @@ def retrieving(license, stats):
     matched = re.match(
         "[A-Z][A-Z][A-Z][A-Z][-][0-9][0-9][-][0-9][0-9][0-9]", license)
     is_match = bool(matched)
+    if len(license) != 12:
+        is_match = False
+    i = 0
+
     if is_match is True:
-        if license in garage:
-            print("Retrieving...\n")
-            garage.remove(license)
-            print("Completed! Good Bye!")
-        elif stats == 0:
-            print("Garage empty!")
+        print("\nLicense plate number correct.\n")
+        while i < (len(garage)):
+            if stats == 0:
+                print("Garage empty!")
+                break
+            if license == garage[i]:
+                print("Retrieving...\n")
+                garage.remove(license)
+                print("Completed! Good Bye!")
+                break
         else:
             print("Car not in garage!")
+        if stats == 0:
+            print("Garage empty!")
     if is_match is False:
-        print("License Incorrect.")
+        print("\nLicense Incorrect.\n")
         license = str(input(
             "Please re-enter your license plate number in ABCD-12-3456 format: "))
         retrieving(license, stats)
@@ -48,33 +76,51 @@ def retrieving(license, stats):
 
 def garage_stats():
     stats = 0
-    for i in garage:
-        if i != None:
+    i = 0
+    while i < len(garage):
+        if garage[i] != None:
             stats += 1
+        i += 1
     print("Cars parked:", stats)
     return stats
 
 
+def cars():
+    i = 0
+    cars_list = ''
+    print("\nCars in garage:")
+    while i < len(garage):
+        if garage[i] != None:
+            cars_list += garage[i] + ('\n')
+        i += 1
+    print(cars_list)
+
+
 def main():
+
     while 1:
         print("\n")
         print("Welcome to Oreoluwa's Garage!\n")
         stats = garage_stats()
-        print("(1) Parking")
+        print("\n(1) Parking")
         print("(2) Retrieving")
         print("(3) Exit\n")
         user_option = str(input("Option: "))
 
         if stats < 10 and user_option == "1":
-            print("Standing By...\n")
-            license = str(input("Enter your license plate number: "))
+            print("\nStanding By...\n")
+            license = str(
+                input("Enter your license plate number: ").strip())
             parking(license)
+            cars()
         elif stats == 10 and user_option == "1":
             print("Garage Full. Please come back later.")
         elif user_option == "2":
-            print("Standing By...\n")
-            license = str(input("Enter your license plate number: "))
+            print("\nStanding By...\n")
+            license = str(
+                input("Enter your license plate number: ").strip())
             retrieving(license, stats)
+            cars()
         elif user_option == "3":
             print("Good bye.\n")
             break
